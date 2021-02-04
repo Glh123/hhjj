5f10d1c7f3718883dcb11aeab0f89d3d560bc6e6
glh <1586498944@example.com>
2021-02-04 19:28:51 +0800

    end

--- src/component/header.js
+++ src/component/header.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './header.css'
 
 export default function Header () {
+
   return (
     <div className='header'>
-      知衣预警推送系统
+      知衣预警推送管理
     </div>
   )
 }
