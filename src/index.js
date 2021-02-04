65e424c8130be69b91653761f0e7a2f72d1d5b09
mac <mac@macdeMacBook-Pro.local>
2021-02-04 13:46:57 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Router, Route } from 'react-router-dom'
 import App from './page/home/index';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
+  // <Router>
+  //   <Route path="/" component={App} />
+  //   <Route path="/home" component={App} />
+  // </Router>,
   document.getElementById('root')
 );
 
